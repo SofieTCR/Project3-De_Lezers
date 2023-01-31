2080a2c5c433470bdb8d33e373215397eb85609b
Sofie <sofiebrink@yahoo.com>
2023-01-31 13:50:53 +0100

    Add homepage to menu

--- Webpage/Menu/menu.php
+++ Webpage/Menu/menu.php
@@ -1,4 +1,5 @@
 <nav style="display: flex; justify-content: space-around; width: 100vw; margin: 0; padding: 0;">
+    <a href="../home/index.php">Home</a>
     <a href="../Sofie/Sofie.php">Sofie's pagina</a>
     <a href="../emily/emily.php">Emily's pagina</a>
     <a href="../rohan/rohan.php">Rohan's pagina</a>
