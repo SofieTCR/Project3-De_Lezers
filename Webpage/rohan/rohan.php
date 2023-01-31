2d4866b9703358b6160d1f1943c273e63eb94124
RohanDayal1 <9019234@student.zadkine.nl>
2023-01-31 13:42:12 +0100

    Update rohan.php

--- Webpage/rohan/rohan.php
+++ Webpage/rohan/rohan.php
@@ -7,6 +7,12 @@
     <title>Rohan</title>
 </head>
 <body>
-    Dit is Rohan's pagina
+<?php
+// Prints the day
+echo date("l") . "<br>";
+
+// Prints the day, date, month, year, time, AM or PM
+echo date("l jS \of F Y h:i:s A") . "<br>";
+?>
 </body>
 </html>
