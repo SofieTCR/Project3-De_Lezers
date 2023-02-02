d9fa7ead98ac205f3b61989ccf79a255099d1b7e
RohanDayal1 <9019234@student.zadkine.nl>
2023-02-02 10:03:02 +0100

    HTML form

--- Webpage/rohan/rohan.php
+++ Webpage/rohan/rohan.php
@@ -9,5 +9,16 @@
 <body>   
 <?php  include("../Menu/menu.php"); ?> 
     <p>Rohan's pagina</p>
+    <h2>HTML Forms</h2>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="pwd">Password:</label> <br>
+  <input type="password" id="pwd" name="pwd" minlength="8"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
 </body>
 </html>
