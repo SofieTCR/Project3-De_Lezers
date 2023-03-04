acdb5737574cf5e0e0ef61f8632772874da9a760
SofieBrink <sofiebrink@yahoo.com>
2023-03-04 14:59:56 +0100

    LeveringRetour 80%

--- Webpage2/Homepage/home.php
+++ Webpage2/Homepage/home.php
@@ -4,6 +4,10 @@
 <body>
     <?php include("../Library/menu.php"); ?>
     <!-- Page Code Goes Here --> 
+
+    <!-- Temp until menu gets done -->
+    <a href="../LeveringRetour/Leveringretour.php">click</a> 
+
     <?php include("../Library/footer.html"); ?>
 </body>
 </html>
