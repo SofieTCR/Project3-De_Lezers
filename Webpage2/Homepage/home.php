a823717b1dc1e6806342f2346e0ff7814f90d8fa
SofieBrink <sofiebrink@yahoo.com>
2023-03-04 14:21:50 +0100

    Footer Made

--- Webpage2/Homepage/home.php
+++ Webpage2/Homepage/home.php
@@ -3,5 +3,7 @@
 <?php include("../Library/head.html"); ?>
 <body>
     <?php include("../Library/menu.php"); ?>
+    <!-- Page Code Goes Here --> 
+    <?php include("../Library/footer.html"); ?>
 </body>
 </html>
