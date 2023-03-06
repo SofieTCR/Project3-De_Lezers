4daa23a190abdeab09794ec419dde60d51c4d741
Emily-Broeksema <9017781@student.zadkine.nl>
2023-03-06 13:04:41 +0100

    Medewerkers

---
+++ Webpage2/Medewerkerspage/Medewerkers.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medewerkers en bedrijf</title>
+</head>
+<body>
+
+    <?php include("../Library/menu.php"); ?>
+
+    <h1>Ons Medewerkers</h1>
+
+    <img src="" alt="Foto van Shondrique"> 
+    <h2>Shondrique</h2>
+
+    <img src="" alt="Foto van Rohan">
+    <h2>Rohan</h2>
+    
+    <img src="" alt="Foto van Sofie">
+    <h2>Sofie</h2>
+
+    <img src="" alt="Foto van Emily">
+    <h2>Emily</h2>
+    
+
+    <h1>Ons Bedrijf</h1>
+
+    <img src="" alt="">
+</body>
+</html>
