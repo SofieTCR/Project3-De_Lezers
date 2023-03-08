0dd904f10f672568a76aa996b91517fb7f3698ce
Sofie <sofiebrink@yahoo.com>
2023-03-08 12:04:04 +0100

    Update Leveringretour.php

--- Webpage2/LeveringRetour/Leveringretour.php
+++ Webpage2/LeveringRetour/Leveringretour.php
@@ -9,12 +9,12 @@
         <section class="LeveringRetour_Section"> <!-- Section about levering -->
             <h1 class="LeveringRetour_Subtitle">Leveringsvoorwaarden:</h1>
             <p class="LeveringRetour_Paragraph">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tincidunt auctor vulputate. Morbi sollicitudin scelerisque convallis. Curabitur hendrerit urna sed sapien rutrum condimentum. Done
+                Bij levering van onze producten zal er een notitie gemaakt worden door de bezorgen in het digitaal leverings system. Mocht u het willen bieden wij ook de mogelijkheid om pakketen aangetekent te versturen. In het geval van een aangetekent pakket zal u gevraagd worden om een handtekening bij ontvangst.
             </p>
             <ul class="LeveringRetour_List">
-                <li class="LeveringRetour_ListItem">Lorum Ipsum</li>
-                <li class="LeveringRetour_ListItem">Dolor sit amet</li>
-                <li class="LeveringRetour_ListItem">Lorum Ipsum</li>
+                <li class="LeveringRetour_ListItem">Bij beschadigde pakketen dient u binnen 5 werkdagen contact met ons op te nemen.</li>
+                <li class="LeveringRetour_ListItem">Als de bezorgings tijd u niet schikt kan u contact op nemen met de courier</li>
+                <li class="LeveringRetour_ListItem">In het geval van een gemiste bezorging zal het pakket bij het dichtsbijzijnde pakketpunt geleverd worden</li>
                 <li class="LeveringRetour_ListItem">Set Amet Auctor</li>
             </ul>
         </section>
