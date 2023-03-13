346616046c37f0aafd88fcc0dc29b6aa6b293224
Sofie <sofiebrink@yahoo.com>
2023-03-13 08:48:52 +0100

    Leveringretour pagina af

--- Webpage2/LeveringRetour/Leveringretour.php
+++ Webpage2/LeveringRetour/Leveringretour.php
@@ -15,19 +15,18 @@
                 <li class="LeveringRetour_ListItem">Bij beschadigde pakketen dient u binnen 5 werkdagen contact met ons op te nemen.</li>
                 <li class="LeveringRetour_ListItem">Als de bezorgings tijd u niet schikt kan u contact op nemen met de courier</li>
                 <li class="LeveringRetour_ListItem">In het geval van een gemiste bezorging zal het pakket bij het dichtsbijzijnde pakketpunt geleverd worden</li>
-                <li class="LeveringRetour_ListItem">Set Amet Auctor</li>
             </ul>
         </section>
         <section class="LeveringRetour_Section"> <!-- Section about retour -->
             <h1 class="LeveringRetour_Subtitle">Retour Policy:</h1>
             <p class="LeveringRetour_Paragraph">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tincidunt auctor vulputate. Morbi sollicitudin scelerisque convallis. Curabitur hendrerit urna sed sapien rutrum condimentum. Done
+                Bij Retour & Omruilen dient u uw bestelling binnen 14 dagen terug te sturen naar De Lezers. Stuur ook een kopie van de factuur en het ingevulde retour formulier mee.
             </p>
             <ul class="LeveringRetour_List">
-                <li class="LeveringRetour_ListItem">Lorum Ipsum</li>
-                <li class="LeveringRetour_ListItem">Dolor sit amet</li>
-                <li class="LeveringRetour_ListItem">Lorum Ipsum</li>
-                <li class="LeveringRetour_ListItem">Set Amet Auctor</li>
+                <li class="LeveringRetour_ListItem">Bij ontvangst krijgt u binnen 10 werkdagen uw geld terug.</li>
+                <li class="LeveringRetour_ListItem">In het geval van omruilen wordt het nieuwe artikel pas verzonden bij ontvangst.</li>
+                <li class="LeveringRetour_ListItem">Als blijkt dat het artikel door u beschadigd is zal het naar u worden teruggestuurd</li>
+                <li class="LeveringRetour_ListItem">In het geval van boekenbundles dienen alle boeken in de bundel terug gestuurd te worden.</li>
             </ul>
         </section>
     </main>
