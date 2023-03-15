2665a19f4f96b6fef3175e4d1cbf5f042356b9a4
Sofie <sofiebrink@yahoo.com>
2023-03-15 11:57:44 +0100

    main medewerkers

--- Webpage2/Medewerkerspage/Medewerkers.php
+++ Webpage2/Medewerkerspage/Medewerkers.php
@@ -7,25 +7,28 @@
 
 <?php include("../Library/menu.php"); ?>
 
+    <main id="med_main">
+        <div class="med-flex-container">
+            <div><img src="" alt="Foto van Shondrique"><h2>Shondrique</h2></div>
+            <div><img src="" alt="Foto van Emily"><h2>Emily</h2></div>
+            <div><img src="" alt="Foto van Sofie"><h2>Sofie</h2></div>
+            <div><img src="" alt="Foto van Rohan"><h2>Rohan</h2></div>  
+        </div>
 
-    <div class="med-flex-container">
-        <div><img src="" alt="Foto van Shondrique"><h2>Shondrique</h2></div>
-        <div><img src="" alt="Foto van Emily"><h2>Emily</h2></div>
-        <div><img src="" alt="Foto van Sofie"><h2>Sofie</h2></div>
-        <div><img src="" alt="Foto van Rohan"><h2>Rohan</h2></div>  
-    </div>
+        
+        
+        
+        <h1 class= "medh1">Ons Bedrijf</h1>
+
+        <p class= "medp">Onze bedrijf De Lezers verkoopt boeken voor gebruikers. Deze boeke zijn speciaal voor Lezers, 
+        dikke boeken met wel 200 pagina's. Onze website heeft een makkelijk te navigeren interface met 
+        zoekfilters om de gebruikers te helpen biedt ook informatie over elk boek, de bladzijden, de 
+        samenvatting en meer. Gebruikers kunnen boeken kopen via de website  </p>
+
+        <img src="" alt="">
+    </main>
 
     
-    
-    
-    <h1 class= "medh1">Ons Bedrijf</h1>
-
-    <p class= "medp">Onze bedrijf De Lezers verkoopt boeken voor gebruikers. Deze boeke zijn speciaal voor Lezers, 
-    dikke boeken met wel 200 pagina's. Onze website heeft een makkelijk te navigeren interface met 
-    zoekfilters om de gebruikers te helpen biedt ook informatie over elk boek, de bladzijden, de 
-    samenvatting en meer. Gebruikers kunnen boeken kopen via de website  </p>
-
-    <img src="" alt="">
 
     <?php include("../Library/footer.html") ?>
 </body>
