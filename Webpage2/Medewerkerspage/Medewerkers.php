c107b84d12855f70df0f26972fc65ddef3a62356
Emily-Broeksema <9017781@student.zadkine.nl>
2023-03-20 13:46:40 +0100

    Medewerkers css + bedrijf foto

--- Webpage2/Medewerkerspage/Medewerkers.php
+++ Webpage2/Medewerkerspage/Medewerkers.php
@@ -9,23 +9,31 @@
 
     <main id="med_main">
         <div class="med-flex-container">
-            <div><img src="" alt="Foto van Shondrique"><h2>Shondrique</h2></div>
-            <div><img src="" alt="Foto van Emily"><h2>Emily</h2></div>
-            <div><img src="" alt="Foto van Sofie"><h2>Sofie</h2></div>
-            <div><img src="" alt="Foto van Rohan"><h2>Rohan</h2></div>  
+            <div><img src="" alt="Foto van Shondrique"><h2 class="medh2">Shondrique</h2> <p class="medpinfo">Lorem ipsum dolor sit amet, ut euismod nulla nulla id dui. Nullam semper, nibh et gravida ullamcorper, mollis gravida sem. Sed eu ante aliquam, eleifend ante vitae, suscipit neque. Donec luctus dolor et augue dapibus, non fringilla nisl pulvinar.</p></div>
+            <div><img src="" alt="Foto van Emily"><h2 class="medh2">Emily</h2><p class="medpinfo">Lorem ipsum dolor sit amet, ut euismod nulla nulla id dui. Nullam semper, nibh et gravida ullamcorper, mollis gravida sem. Sed eu ante aliquam, eleifend ante vitae, suscipit neque. Donec luctus dolor et augue dapibus, non fringilla nisl pulvinar.</p></div>
+            <div><img src="" alt="Foto van Sofie"><h2 class="medh2">Sofie</h2><p class="medpinfo">Lorem ipsum dolor sit amet, ut euismod nulla nulla id dui. Nullam semper, nibh et gravida ullamcorper, mollis gravida sem. Sed eu ante aliquam, eleifend ante vitae, suscipit neque. Donec luctus dolor et augue dapibus, non fringilla nisl pulvinar.</p></div>
+            <div><img src="" alt="Foto van Rohan"><h2 class="medh2">Rohan</h2><p class="medpinfo">Lorem ipsum dolor sit amet, ut euismod nulla nulla id dui. Nullam semper, nibh et gravida ullamcorper, mollis gravida sem. Sed eu ante aliquam, eleifend ante vitae, suscipit neque. Donec luctus dolor et augue dapibus, non fringilla nisl pulvinar.</p></div>  
         </div>
 
         
         
         
         <h1 class= "medh1">Ons Bedrijf</h1>
-
-        <p class= "medp">Onze bedrijf De Lezers verkoopt boeken voor gebruikers. Deze boeke zijn speciaal voor Lezers, 
+        
+        <div class="meddiv">
+            <div class="medpdiv">
+                <p class= "medp">Onze bedrijf De Lezers verkoopt boeken voor gebruikers. Deze boeke zijn speciaal voor Lezers, 
         dikke boeken met wel 200 pagina's. Onze website heeft een makkelijk te navigeren interface met 
         zoekfilters om de gebruikers te helpen biedt ook informatie over elk boek, de bladzijden, de 
         samenvatting en meer. Gebruikers kunnen boeken kopen via de website  </p>
+            </div>
+        
 
-        <img src="" alt="bedrijf">
+        <img class="medimg" src="./bedrijf.png" alt="bedrijf">
+
+        </div>
+        
+
     </main>
 
     
