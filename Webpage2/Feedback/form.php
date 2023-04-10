1525afbb1017f1a071e061eb472fb074e4d146a3
RohanDayal1 <9019234@student.zadkine.nl>
2023-04-10 16:55:10 +0200

    form css + mdw rohan

--- Webpage2/Feedback/form.php
+++ Webpage2/Feedback/form.php
@@ -1,55 +1,63 @@
 <?php
-// connect database 
-$servername = "localhost";
-$username = "username";
+$hostName = "localhost";
+$username = "root";
 $password = "";
-$dbname = "de_lezers";
+$databaseName = "de_lezers";
 
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn = new PDO("mysql:host=$hostName;dbname=$databaseName", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
   // get data
   $naam = $_POST['naam'];
   $email = $_POST['email'];
   $onderwerp = $_POST['onderwerp'];
   $bericht = $_POST['bericht'];
-  
-  // data in de database
-  $stmt = $conn->prepare("INSERT INTO klachten (naam, email, onderwerp, bericht) VALUES (:naam, :email, :onderwerp, :bericht)");
-  $stmt->bindParam(':naam', $naam);
-  $stmt->bindParam(':email', $email);
-  $stmt->bindParam(':onderwerp', $onderwerp);
-  $stmt->bindParam(':bericht', $bericht);
-  $stmt->execute();
 
-  header('Location: index.php');
+  // check if bericht field is not empty
+  if (!empty($bericht)) {
+    // data in de database
+    $stmt = $conn->prepare("INSERT INTO klachten (naam, email, onderwerp, bericht) VALUES (:naam, :email, :onderwerp, :bericht)");
+    $stmt->bindParam(':naam', $naam);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':onderwerp', $onderwerp);
+    $stmt->bindParam(':bericht', $bericht);
+    $stmt->execute();
+  }
+
+  header("Location: ../Homepage/home.php");
   exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feedback</title>
-    <link rel="stylesheet" href="style.css">
+  <?php include("../Library/head.html"); ?>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Feedback</title>
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <div class="container">
-        <form action="function.php" method="POST">
-            <label for="naam">Naam:</label> <br>
-            <input type="text" name="naam" id="naam"><br>
-            <label for="email">Email:</label><br>
-            <input type="email" name="email" id="email"><br>
-            <label for="onderwerp">Onderwerp:</label><br>
-            <input type="text" name="onderwerp" id="onderwerp"><br>
-            <label for="bericht">Bericht:</label><br>
-            <textarea name="bericht" cols="30" rows="10"></textarea><br>
-            <input type="submit" value="Send"><br>
-        </form>
+  <?php include("../Library/menu.php"); ?>
+  <h1 class="formh1">Laat alstublijft uw feedback achter, zodat we onze services verbeteren!</h1>
+  <main id="Form_Main">
+    <div id="Form_Div">
+      <form method="POST"> <br> <br>
+        <div class="Form_Row"><label for="naam">Naam:</label> <input type="text" name="naam" id="naam"></div><br> <br>
+        <div class="Form_Row"><label for="email">Email:</label> <input type="email" name="email" id="email"></div><br> <br>
+        <div class="Form_Row"><label for="onderwerp">Onderwerp:</label> <input type="text" name="onderwerp" id="onderwerp"></div><br> <br> <br>
+        <div class="Form_Row"><label for="bericht">Bericht:</label> <textarea id="message" name="bericht" rows="5" required></textarea></div><br> <br>
+        <div class="Form_Row"><input id="Form_Submit" type="submit" name="submit" value="Versturen">
+      </form>
     </div>
+  </main>
+  <?php include("../Library/footer.html"); ?>
 </body>
+
 </html>
