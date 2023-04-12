d7978129f57bbd898894a2466878f715ebf2ddb6
Shondrique-dejon <9018540@student.zadkine.nl>
2023-04-12 09:34:38 +0200

    milleu pagina update

--- Webpage2/Milleu/milleu.php
+++ Webpage2/Milleu/milleu.php
@@ -4,24 +4,22 @@
 <body>
     <?php include("../Library/menu.php"); ?>
     
-    <main id="Homepage_Main">
-        
-     <Section>
-
-     <h1 class="Milleu_Subtitle">Milleu:</h1><br>
-
-     <p id="Milleu_Main">Als boekenbedrijf is het belangrijk om een sterk milieu beleid te hebben en actief bij te dragen aan een duurzame toekomst. Hierbij gaat het niet alleen om het verminderen van de ecologische voetafdruk, maar ook om het bijdragen aan de maatschappij en het verbeteren van het welzijn van medewerkers en klanten.
-
+    <main id="Milleu_Main">
+        <section class="Milleu_Section"> <!-- Section about Milleu -->
+            <h1 class="Milleu_Subtitle">Milleu:</h1>
+            <p class="Milleu_Paragraph">
+            Als boekenbedrijf is het belangrijk om een sterk milieu beleid te hebben en actief bij te dragen aan een duurzame toekomst. Hierbij gaat het niet alleen om het verminderen van de ecologische voetafdruk, maar ook om het bijdragen aan de maatschappij en het verbeteren van het welzijn van medewerkers en klanten.
 Een belangrijk onderdeel van het milieu beleid is het verminderen van de CO2-uitstoot en het gebruik van duurzame energiebronnen. 
 <br> Dit kan bijvoorbeeld door het verminderen van het energieverbruik van het bedrijf,  het gebruik van hernieuwbare energiebronnen zoals zonne-energie en windenergie,  en het gebruik van milieuvriendelijke transportmethoden. <br> Daarnaast kan het bedrijf zich richten op het verminderen van de hoeveelheid afval die wordt geproduceerd en het stimuleren van recycling.
-
 Ook kan het boekenbedrijf bijdragen aan de maatschappij door het steunen van lokale gemeenschappen en  het bieden van mogelijkheden voor sociale en duurzame projecten. <br> Het bedrijf kan bijvoorbeeld samenwerken met lokale organisaties die zich richten op het verbeteren van de leefomstandigheden en het milieu.
-
-Tot slot is het belangrijk om het welzijn van medewerkers en klanten in het oog te houden. <br>  Het bedrijf kan bijvoorbeeld werken aan een veilige en gezonde werkomgeving voor medewerkers, en het bieden van gezonde en duurzame producten voor klanten.</p>
-     </Section>
+               </p>
+            <ul class="Milleu_List">
+                <li class="Milleu_ListItem">Het bedrijf kan bijvoorbeeld samenwerken met lokale organisaties die zich richten op het verbeteren van de leefomstandigheden en het milieu.</li>
+                <li class="Milleu_ListItem">Het bedrijf kan bijvoorbeeld werken aan een veilige en gezonde werkomgeving voor medewerkers, en het bieden van gezonde en duurzame producten voor klanten.</li>
+                <li class="Milleu_ListItem">Tot slot is het belangrijk om het welzijn van medewerkers en klanten in het oog te houden</li>
+            </ul>
+        </section>
     </main>
-  
-
     <?php include("../Library/footer.html"); ?>
 </body>
 </html>
